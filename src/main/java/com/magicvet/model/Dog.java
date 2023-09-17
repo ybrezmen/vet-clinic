0f0a73aa3eb2444ac92428f39b71b2caf0fc2bf4
yuriibrezmen <prometheus.ybrezmen@gmail.com>
2023-09-17 22:44:43 +0300

    REQ-06: Sort dogs by size

--- src/main/java/com/magicvet/model/Dog.java
+++ src/main/java/com/magicvet/model/Dog.java
@@ -2,7 +2,19 @@ package main.java.com.magicvet.model;
 
 public class Dog extends Pet {
 
+	public static final String XS = "XS";
+	public static final String S = "S";
+	public static final String M = "M";
+	public static final String L = "L";
+	public static final String XL = "XL";
+
 	private String size;
+
+	public Dog() { }
+
+	public Dog(String size) {
+		this.size = size;
+	}
 
 	public void setSize(String size) {
 		this.size = size;
