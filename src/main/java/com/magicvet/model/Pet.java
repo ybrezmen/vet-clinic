8074b20811711a9cb7c2cc5b8d98dd2d5f8cbf53
yuriibrezmen <prometheus.ybrezmen@gmail.com>
2023-09-20 22:19:11 +0300

    REQ-08: Add registration date for Pet class

--- src/main/java/com/magicvet/model/Pet.java
+++ src/main/java/com/magicvet/model/Pet.java
@@ -1,12 +1,18 @@
 package main.java.com.magicvet.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public abstract class Pet {
+
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
 
 	private String type;
 	private String sex;
 	private String age;
 	private String name;
 	private String ownerName;
+	private final LocalDateTime registrationDate = LocalDateTime.now();
 
 	@Override
 	public String toString() {
@@ -16,6 +22,7 @@ public abstract class Pet {
 				+ ", age = " + age
 				+ ", name = " + name
 				+ ", ownerName = " + ownerName
+				+ ", registrationDate = " + registrationDate.format(FORMATTER)
 				+ "}";
 	}
 
