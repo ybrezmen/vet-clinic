a884dca7ef8c8344f829799d20ecadb11d4cf29c
yuriibrezmen <prometheus.ybrezmen@gmail.com>
2023-10-05 18:23:47 +0300

    Refactor to use Streams & Optional

--- src/main/java/com/magicvet/component/EntityRegister.java
+++ src/main/java/com/magicvet/component/EntityRegister.java
@@ -7,9 +7,10 @@ import main.java.com.magicvet.service.ClientService;
 import main.java.com.magicvet.service.PetService;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class EntityRegister {
 
@@ -21,13 +22,13 @@ public class EntityRegister {
 
 		String message = "Do you want to register more clients? (y/n): ";
 		do {
-			Client client = addClient();
-			if (client != null) {
-				clients.add(client);
-			}
+			Optional<Client> client = addClient();
+			client.ifPresent(clients::add);
 		} while (verifyRepeating(message));
 
-		Map<Client.Location, List<Client>> clientsByLocation = groupClients(clients);
+		Map<Client.Location, List<Client>> clientsByLocation = clients.stream()
+				.collect(Collectors.groupingBy(Client::getLocation));
+
 		printClients(clientsByLocation);
 	}
 
@@ -41,37 +42,9 @@ public class EntityRegister {
 		}
 	}
 
-	private Map<Client.Location, List<Client>> groupClients(List<Client> clients) {
-		List<Client> fromKyiv = new ArrayList<>();
-		List<Client> fromLviv = new ArrayList<>();
-		List<Client> fromOdesa = new ArrayList<>();
-		List<Client> unknownLocation = new ArrayList<>();
-
-		for (Client client : clients) {
-			switch (client.getLocation()) {
-				case KYIV -> fromKyiv.add(client);
-				case LVIV -> fromLviv.add(client);
-				case ODESA -> fromOdesa.add(client);
-				case UNKNOWN -> unknownLocation.add(client);
-			}
-		}
-
-		Map<Client.Location, List<Client>> clientsByLocation = new HashMap<>();
-		clientsByLocation.put(Client.Location.KYIV, fromKyiv);
-		clientsByLocation.put(Client.Location.LVIV, fromLviv);
-		clientsByLocation.put(Client.Location.ODESA, fromOdesa);
-		clientsByLocation.put(Client.Location.UNKNOWN, unknownLocation);
-
-		return clientsByLocation;
-	}
-
-	private Client addClient() {
-		Client client = clientService.registerNewClient();
-
-		if (client != null) {
-			registerPets(client);
-			System.out.println(client);
-		}
+	private Optional<Client> addClient() {
+		Optional<Client> client = clientService.registerNewClient();
+		client.ifPresent(this::registerPets);
 
 		return client;
 	}
@@ -80,6 +53,7 @@ public class EntityRegister {
 		String message = "Do you want to add more pets for the current client? (y/n): ";
 		do {
 			addPet(client);
+			System.out.println(client);
 		} while (verifyRepeating(message));
 	}
 
--- src/main/java/com/magicvet/service/ClientService.java
+++ src/main/java/com/magicvet/service/ClientService.java
@@ -3,6 +3,7 @@ package main.java.com.magicvet.service;
 import main.java.com.magicvet.Main;
 import main.java.com.magicvet.model.Client;
 
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,7 +11,7 @@ public class ClientService {
 
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
-	public Client registerNewClient() {
+	public Optional<Client> registerNewClient() {
 		Client client = null;
 
 		System.out.println("Please provide client details.");
@@ -26,7 +27,7 @@ public class ClientService {
 			System.out.println("Provided email is invalid.");
 		}
 
-		return client;
+		return Optional.ofNullable(client);
 	}
 
 	private static Client buildClient(String email) {
